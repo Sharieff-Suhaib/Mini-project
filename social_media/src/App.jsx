6bdd73190096a0b59900fac15a80092e8ac37a16
SK <sivakumar.smsd@gmail.com>
2024-11-03 17:23:39 +0530

    sk

--- social_media/src/App.jsx
+++ social_media/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Register from "./components/Register"
 import Home from "./components/Home"
 import Login from "./components/Login"
+import Post2 from "./components/post2";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App(){
     
@@ -11,6 +12,7 @@ function App(){
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login/>}/>
             <Route path="/home" element={<Home/>}/>
+            <Route path="/post2" element={<Post2/>}/>
         </Routes>  
     </Router>
     );
