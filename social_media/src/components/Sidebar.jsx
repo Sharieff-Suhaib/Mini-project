7db46f5f4642eceaf042129035feb139e55a91a9
SK <sivakumar.smsd@gmail.com>
2024-11-04 20:45:03 +0530

    newchange

--- social_media/src/components/Sidebar.jsx
+++ social_media/src/components/Sidebar.jsx
@@ -8,8 +8,8 @@ const Sidebar = () => {
         navigate("/friends");
     }
     return (
-        <div className="bg-gray-100 w-64 p-4">
-            <div className="flex flex-col space-y-2">
+        <div >
+            <div >
                 <div onClick={handleFriend} className="p-2 hover:bg-gray-200 rounded">Friends</div>
                 <div className="p-2 hover:bg-gray-200 rounded">Groups</div>
                 <div className="p-2 hover:bg-gray-200 rounded">Marketplace</div>
