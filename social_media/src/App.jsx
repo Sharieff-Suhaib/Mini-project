88f388aae08f7968692cd0464ade6c4da12242af
SK <sivakumar.smsd@gmail.com>
2024-11-04 20:57:19 +0530

    newchange

--- social_media/src/App.jsx
+++ social_media/src/App.jsx
@@ -3,10 +3,9 @@ import Register from "./components/Register"
 import Home from "./components/Home"
 import Login from "./components/Login"
 import Post2 from "./components/post2";
-=======
 import Friend from "./components/Friend"
 import Chat from "./components/Chat"
->>>>>>> 9b5b18b4aa52feb3c1750cb42f7201e8a8d72384
+import Profile from "./components/Profile"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App(){
     return(
@@ -16,11 +15,10 @@ function App(){
             <Route path="/login" element={<Login/>}/>
             <Route path="/home" element={<Home/>}/>
             <Route path="/post2" element={<Post2/>}/>
-=======
             <Route path="/profile" element={<Profile/>}/>
             <Route path="/friends" element={<Friend/>}/>
             <Route path="/chat" element={<Chat/>}/>
->>>>>>> 9b5b18b4aa52feb3c1750cb42f7201e8a8d72384
+
         </Routes>  
     </Router>
     );
